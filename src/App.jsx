f8bbfa8f22696a9b4168e8521cfb8496f38cfee5
Dacron <favouradebimpe63@gmail.com>
2024-01-24 21:34:30 +0100

    some changes...

--- src/App.jsx
+++ src/App.jsx
@@ -15,59 +15,74 @@ import Servicesmobile from "./Components/Servicesmobile";
 import chicken from "/src/images/cute chicken-rafiki.png";
 import programmer from "/src/images/Programmer-pana.png";
 
+
 function App() {
   return (
-    <Box className="background">
-      {/* <LoadingComponent /> */}
-      <Box
-        component={motion.div}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, y: 0, transition: { delay: 4 } }}
-      >
-        <Parallax
-          pages={6}
-          style={{ backgroundColor: "#919191", color: "#040404" }}
+    <>
+
+      <>
+        {/* <LoadingComponent /> */}
+
+        <Box
+        // component={motion.div}
+        // initial={{ opacity: 0 }}
+        // animate={{ opacity: 1, y: 0, transition: { delay: 4 } }}
         >
-          <Name />
-
-          <Talk />
-
-          <About />
-
-          <Services />
-          <Servicesmobile />
-
-          <Projects />
-
-          <Contact />
-
-          <ParallaxLayer
-            offset={1.3}
-            speed={-0.3}
-            style={{ pointerEvents: "none" }}
+          <Parallax
+            pages={6}
+            style={{
+              backgroundColor: "#919191",
+              color: "#040404",
+              height: "100vh",
+            }}
           >
-            <Box
-              component={animated.img}
-              whileHover={{ scale: 1.2 }}
-            src={chicken}
-              sx={{ width: {xs:"40%", sm:"30%",lg:"15%"}, marginLeft: {xs:"66%",sm:"70%" }}}
-            />
-          </ParallaxLayer>
+            <Name />
 
-          <ParallaxLayer
-            offset={3.8}
-            speed={0.6}
-            style={{ pointerEvents: "none" }}
-          >
-            <Box
-              component={motion.img}
-              src={programmer}
-              sx={{ width: {xs:"52%", sm:"34%",lg:"15%"}, marginLeft: {xs:"50%",sm:"10%" }}}
-            />
-          </ParallaxLayer>
-        </Parallax>
-      </Box>
-    </Box>
+            <Talk />
+
+            <About />
+
+            <Services />
+            <Servicesmobile />
+
+            <Projects />
+
+            <Contact />
+
+            <ParallaxLayer
+              offset={1.3}
+              speed={-0.3}
+              style={{ pointerEvents: "none" }}
+            >
+              <Box
+                component={animated.img}
+                whileHover={{ scale: 1.2 }}
+                src={chicken}
+                sx={{
+                  width: { xs: "40%", sm: "30%", lg: "15%" },
+                  marginLeft: { xs: "66%", sm: "70%" },
+                }}
+              />
+            </ParallaxLayer>
+
+            <ParallaxLayer
+              offset={3.8}
+              speed={0.6}
+              style={{ pointerEvents: "none" }}
+            >
+              <Box
+                component={motion.img}
+                src={programmer}
+                sx={{
+                  width: { xs: "52%", sm: "34%", lg: "15%" },
+                  marginLeft: { xs: "50%", sm: "10%" },
+                }}
+              />
+            </ParallaxLayer>
+          </Parallax>
+        </Box>
+      </>
+    </>
   );
 }
 
