0562448a27cdc64def129b6655fbd9691aa68eec
Dacron <favouradebimpe63@gmail.com>
2024-01-30 19:02:13 +0100

    little fixes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -45,7 +45,7 @@ function Contact() {
 
   const theme = createTheme({
     typography: {
-      fontFamily: 'Raleway, sans-serif',
+      fontFamily: "Raleway, sans-serif",
     },
     palette: {
       primary: {
@@ -101,196 +101,200 @@ function Contact() {
   };
   return (
     <ThemeProvider theme={theme}>
-      <ParallaxLayer offset={5} speed={1.2} style={{ backgroundColor: "white" }}>
-      <Box
-        component={motion.div}
-        initial={{ opacity: 0, y: 30 }}
-        whileInView={{
-          opacity: 1,
-          y: 0,
-          transition: { delay: 0.1, duration: 0.8 },
-        }}
-        sx={{
-          width: { sm: "92%" },
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          width: "100%",
-        }}
+      <ParallaxLayer
+        offset={5}
+        speed={1.2}
+        style={{ backgroundColor: "white" }}
       >
-        <Stack
+        <Box
           component={motion.div}
           initial={{ opacity: 0, y: 30 }}
           whileInView={{
             opacity: 1,
             y: 0,
-            transition: { delay: 0.5, duration: 1 },
+            transition: { delay: 0.1, duration: 0.8 },
           }}
-          spacing={{ xs: 4, sm: 2, lg: 4 }}
+          sx={{
+            width: { sm: "92%" },
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            width: "100%",
+          }}
         >
-          <Typography
-            variant="h1"
-            sx={{
-              textAlign: "center",
-              fontSize: { xs: "70px", sm: "80px", lg: "120px" },
-            }}
-          >
-            {" "}
-            Contact{" "}
-          </Typography>
-
           <Stack
-            spacing={{ sm: 10, lg: 40 }}
-            direction={{ lg: "row" }}
-            sx={{ alignItems: "center" }}
+            component={motion.div}
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: { delay: 0.5, duration: 1 },
+            }}
+            spacing={{xs:2, sm: 2, lg: 4 }}
           >
-            <Stack spacing={3}>
-              <Stack
-                spacing={2}
-                sx={{ textAlign: { xs: "center", sm: "initial" } }}
-              >
-                <Typography
-                  variant="h1"
-                  sx={{ fontSize: { xs: "30px", sm: "48px" } }}
-                >
-                  Got a brilliant concept?{" "}
-                </Typography>
-                <Typography
-                  variant="h2"
-                  sx={{ fontSize: { xs: "20px", sm: "40px" } }}
-                >
-                  Let's make it a reality.
-                </Typography>
-              </Stack>
+            <Typography
+              variant="h1"
+              sx={{
+                textAlign: "center",
+                fontSize: { xs: "68px", sm: "80px", lg: "120px" },
+              }}
+            >
+              {" "}
+              Contact{" "}
+            </Typography>
 
-              {/* FORM FOR EMAIL JS */}
-              <form ref={form} onSubmit={sendEmail}>
+            <Stack
+              spacing={{ sm: 10, lg: 40 }}
+              direction={{ lg: "row" }}
+              sx={{ alignItems: "center" }}
+            >
+              <Stack spacing={3}>
                 <Stack
-                  spacing={3}
-                  sx={{
-                    width: { sm: "88%", lg: "100%" },
-                    justifyContent: "center",
-                    padding: { xs: "20px", sm: 0 },
-                  }}
+                  spacing={2}
+                  sx={{ textAlign: { xs: "center", sm: "initial" } }}
                 >
-                  <Stack direction="row" spacing={3}>
-                    <ThemeProvider theme={theme}>
-                      <TextField
-                        variant="standard"
-                        type="name"
-                        label="Your Name"
-                        color="primary"
-                        name="user_name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                      />
-                    </ThemeProvider>
-                    <ThemeProvider theme={theme}>
-                      <TextField
-                        variant="standard"
-                        type="email"
-                        label="Your Email"
-                        color="primary"
-                        name="user_email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                      />
-                    </ThemeProvider>
-                  </Stack>
-                  <ThemeProvider theme={theme}>
-                    <TextField
-                      variant="standard"
-                      type="text"
-                      label="Your Message"
-                      multiline
-                      color="primary"
-                      name="message"
-                      value={message}
-                      onChange={(e) => setMessage(e.target.value)}
-                    />
-                  </ThemeProvider>
-                  <LoadingButton
-                    loading={sending}
-                    variant="contained"
-                    sx={{
-                      textTransform: "none",
-                      width: { xs: "30%", sm: "20%", lg: "34%" },
-                      backgroundColor: "#303030",
-                      "&:hover": {
-                        backgroundColor: "#040404",
-                      },
-                    }}
-                    type="submit"
+                  <Typography
+                    variant="h1"
+                    sx={{ fontSize: { xs: "30px", sm: "48px" } }}
                   >
-                    Send
-                  </LoadingButton>
+                    Got a brilliant concept?{" "}
+                  </Typography>
+                  <Typography
+                    variant="h2"
+                    sx={{ fontSize: { xs: "20px", sm: "40px" } }}
+                  >
+                    Let's make it a reality.
+                  </Typography>
                 </Stack>
-              </form>
-            </Stack>
 
-            <Stack spacing={2}>
-              {/* STACK FOR CONTACT DETAILS */}
-              <Stack spacing={2}>
-                <Typography variant="h3">Contact Details</Typography>
-                <Stack
-                  direction="row"
-                  spacing={1}
-                  sx={{ alignItems: "center" }}
-                >
-                  <EmailIcon />
-                  <a href="mailto:favouradebimpe63@gmail.com">
-                    <Typography sx={{ color: "black" }}>
-                      favouradebimpe@gmail.com
-                    </Typography>
-                  </a>
-                </Stack>
+                {/* FORM FOR EMAIL JS */}
+                <form ref={form} onSubmit={sendEmail}>
+                  <Stack
+                    spacing={3}
+                    sx={{
+                      width: { sm: "88%", lg: "100%" },
+                      justifyContent: "center",
+                      padding: { xs: "20px", sm: 0 },
+                    }}
+                  >
+                    <Stack direction="row" spacing={3}>
+                      <ThemeProvider theme={theme}>
+                        <TextField
+                          variant="standard"
+                          type="name"
+                          label="Your Name"
+                          color="primary"
+                          name="user_name"
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
+                        />
+                      </ThemeProvider>
+                      <ThemeProvider theme={theme}>
+                        <TextField
+                          variant="standard"
+                          type="email"
+                          label="Your Email"
+                          color="primary"
+                          name="user_email"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
+                      </ThemeProvider>
+                    </Stack>
+                    <ThemeProvider theme={theme}>
+                      <TextField
+                        variant="standard"
+                        type="text"
+                        label="Your Message"
+                        multiline
+                        color="primary"
+                        name="message"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                      />
+                    </ThemeProvider>
+                    <LoadingButton
+                      loading={sending}
+                      variant="contained"
+                      sx={{
+                        textTransform: "none",
+                        width: { xs: "30%", sm: "20%", lg: "34%" },
+                        backgroundColor: "#303030",
+                        "&:hover": {
+                          backgroundColor: "#040404",
+                        },
+                      }}
+                      type="submit"
+                    >
+                      Send
+                    </LoadingButton>
+                  </Stack>
+                </form>
               </Stack>
+
               <Stack spacing={2}>
-                <Typography variant="h3">Digital Spaces</Typography>
-                <Stack
-                  direction="row"
-                  spacing={1}
-                  sx={{ alignItems: "center" }}
-                >
-                  <GitHubIcon />{" "}
-                  <a href="https://github.com/Proton-number">
-                    <Typography sx={{ color: "black" }}>Github</Typography>
-                  </a>
+                {/* STACK FOR CONTACT DETAILS */}
+                <Stack spacing={2}>
+                  <Typography variant="h3">Contact Details</Typography>
+                  <Stack
+                    direction="row"
+                    spacing={1}
+                    sx={{ alignItems: "center" }}
+                  >
+                    <EmailIcon />
+                    <a href="mailto:favouradebimpe63@gmail.com">
+                      <Typography sx={{ color: "black" }}>
+                        favouradebimpe@gmail.com
+                      </Typography>
+                    </a>
+                  </Stack>
                 </Stack>
-                <Stack
-                  direction="row"
-                  spacing={1}
-                  sx={{ alignItems: "center" }}
-                >
-                  <Box
-                    component="img"
-                    sx={{ width: { sm: "24px", lg: "25px" } }}
-                    src={NetlifyIcon}
-                    alt="Netlify Icon"
-                  />
-                  <a href="https://app.netlify.com/teams/favouradebimpe63/overview">
-                    <Typography sx={{ color: "black" }}>Netlify</Typography>
-                  </a>
+                <Stack spacing={2}>
+                  <Typography variant="h3">Digital Spaces</Typography>
+                  <Stack
+                    direction="row"
+                    spacing={1}
+                    sx={{ alignItems: "center" }}
+                  >
+                    <GitHubIcon />{" "}
+                    <a href="https://github.com/Proton-number">
+                      <Typography sx={{ color: "black" }}>Github</Typography>
+                    </a>
+                  </Stack>
+                  <Stack
+                    direction="row"
+                    spacing={1}
+                    sx={{ alignItems: "center" }}
+                  >
+                    <Box
+                      component="img"
+                      sx={{ width: { sm: "24px", lg: "25px" } }}
+                      src={NetlifyIcon}
+                      alt="Netlify Icon"
+                    />
+                    <a href="https://app.netlify.com/teams/favouradebimpe63/overview">
+                      <Typography sx={{ color: "black" }}>Netlify</Typography>
+                    </a>
+                  </Stack>
                 </Stack>
-              </Stack>
-              <Stack direction="row" spacing={2}>
-                <Typography> {formattedDate}</Typography>
-                <Typography> {formattedTime}</Typography>
-                <Typography> {level * 100.0}%</Typography>
+                <Stack direction="row" spacing={2}>
+                  <Typography> {formattedDate}</Typography>
+                  <Typography> {formattedTime}</Typography>
+                  <Typography> {level * 100.0}%</Typography>
+                </Stack>
               </Stack>
             </Stack>
           </Stack>
-        </Stack>
-        <Snackbar
-          open={open}
-          autoHideDuration={2800}
-          onClose={handleClose}
-          message="Message sent"
-          sx={{ width: { xs: "30%", sm: "50%", lg: "100%" } }}
-        />
-      </Box>
-    </ParallaxLayer>
+          <Snackbar
+            open={open}
+            autoHideDuration={2800}
+            onClose={handleClose}
+            message="Message sent"
+            sx={{ width: { xs: "30%", sm: "50%", lg: "100%" } }}
+          />
+        </Box>
+      </ParallaxLayer>
     </ThemeProvider>
   );
 }
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -18,75 +18,75 @@ function Services() {
   return (
     <ThemeProvider theme={font}>
       <Box id="service-desktop">
-      <ParallaxLayer
-        offset={3}
-        speed={1.1}
-        factor={1.4}
-        style={{ backgroundColor: "white" }}
-      >
-        <Stack
-          component={motion.div}
-          initial={{ opacity: 0, y: 55 }}
-          whileInView={{
-            opacity: 1,
-            y: 0,
-            transition: { delay: 0.4, duration: 1.1 },
-          }}
-          sx={{
-            width: { sm: "92%", lg: "84%" },
-            margin: "auto",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "100vh",
-          }}
-          spacing={{ sm: 4, lg: 3 }}
+        <ParallaxLayer
+          offset={3}
+          speed={1.1}
+          factor={1.4}
+          style={{ backgroundColor: "white" }}
         >
-          <Typography variant="h1" sx={{ textAlign: "center" }}>
-            Services
-          </Typography>
-          <Stack direction={{ sm: "row" }}>
-            <Stack spacing={{ sm: 8 }}>
-              <Stack spacing={{ sm: 2 }}>
-                <Typography variant="h3">My Expertises</Typography>
-                <Typography
-                  variant="h6"
-                  sx={{ width: { sm: "80%", lg: "50%" } }}
-                >
-                  I focus on everything web design and my goal is to deliver a
-                  very good digital experience for everyone.
-                </Typography>
+          <Stack
+            component={motion.div}
+            initial={{ opacity: 0, y: 55 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: { delay: 0.4, duration: 1.1 },
+            }}
+            sx={{
+              width: { sm: "92%", lg: "84%" },
+              margin: "auto",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "100vh",
+            }}
+            spacing={{ sm: 4, lg: 3 }}
+          >
+            <Typography variant="h1" sx={{ textAlign: "center" }}>
+              Services
+            </Typography>
+            <Stack direction={{ sm: "row" }}>
+              <Stack spacing={{ sm: 8 }}>
+                <Stack spacing={{ sm: 2 }}>
+                  <Typography variant="h3">My Expertises</Typography>
+                  <Typography
+                    variant="h6"
+                    sx={{ width: { sm: "80%", lg: "50%" } }}
+                  >
+                    I focus on everything web design and my goal is to deliver a
+                    very good digital experience for everyone.
+                  </Typography>
+                </Stack>
+                <Stack spacing={{ sm: 2 }}>
+                  <Typography variant="h3">Tech Stack</Typography>
+                  <Typography
+                    variant="h6"
+                    sx={{ width: { sm: "80%", lg: "50%" } }}
+                  >
+                    These are my current tech stack when i want any project to
+                    happen. I am open to learn more and i'm still learning more
+                    on the ones i already use.
+                  </Typography>
+                </Stack>
               </Stack>
-              <Stack spacing={{ sm: 2 }}>
-                <Typography variant="h3">Tech Stack</Typography>
-                <Typography
-                  variant="h6"
-                  sx={{ width: { sm: "80%", lg: "50%" } }}
-                >
-                  These are my current tech stack when i want any project to
-                  happen. I am open to learn more and i'm still learning more on
-                  the ones i already use.
-                </Typography>
-              </Stack>
-            </Stack>
-            <Stack spacing={{ sm: 4 }}>
-              <Stack>
-                <Typography variant="h3">Web development</Typography>
-                <Typography variant="h3">Web design</Typography>
-              </Stack>
-              <Stack>
-                <Typography variant="h3">HTML</Typography>
-                <Typography variant="h3">CSS</Typography>
-                <Typography variant="h3">JavaScript</Typography>
-                <Typography variant="h3">React</Typography>
-                <Typography variant="h3">Material UI</Typography>
-                <Typography variant="h3">Framer Motion</Typography>
-                <Typography variant="h3">Sanity</Typography>
+              <Stack spacing={{ sm: 4 }}>
+                <Stack>
+                  <Typography variant="h3">Web development</Typography>
+                  <Typography variant="h3">Web design</Typography>
+                </Stack>
+                <Stack>
+                  <Typography variant="h3">HTML</Typography>
+                  <Typography variant="h3">CSS</Typography>
+                  <Typography variant="h3">JavaScript</Typography>
+                  <Typography variant="h3">React</Typography>
+                  <Typography variant="h3">Material UI</Typography>
+                  <Typography variant="h3">Framer Motion</Typography>
+                  <Typography variant="h3">Sanity</Typography>
+                </Stack>
               </Stack>
             </Stack>
           </Stack>
-        </Stack>
-      </ParallaxLayer>
-    </Box>
+        </ParallaxLayer>
+      </Box>
     </ThemeProvider>
   );
 }
